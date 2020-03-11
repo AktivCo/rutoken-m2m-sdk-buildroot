631a77fd12296f3da0ac76221f31b563f822d68c
Eugene Mironenko <mironenko@rutoken.ru>
2020-03-11 21:37:32 +0300

    Add m2m-utils package

---
+++ package/rutoken-m2m-sdk/m2m-utils/m2m-utils.mk
@@ -0,0 +1,29 @@
+################################################################################
+#
+# M2M utils
+#
+################################################################################
+RUTOKEN_M2M_UTILS_VERSION = build
+RUTOKEN_M2M_UTILS_SOURCE      = m2m-utils-$(RUTOKEN_M2M_UTILS_VERSION).tar.gz
+RUTOKEN_M2M_UTILS_SITE_METHOD = git
+RUTOKEN_M2M_UTILS_SITE        = git@scm.aktivco.ru:rutoken-res/m2m-utils.git
+RUTOKEN_M2M_UTILS_INSTALL_DIR = $(TARGET_DIR)/opt/Rutoken/m2m-utils
+RUTOKEN_M2M_UTILS_API_INSTALL_DIR = $(TARGET_DIR)/opt/Rutoken/m2m-utils/include
+RUTOKEN_M2M_UTILS_README_INSTALL_DIR = $(TARGET_DIR)/opt/Rutoken/m2m-utils/doc/
+RUTOKEN_M2M_UTILS_LICENSE = BSD-2-Clause
+RUTOKEN_M2M_UTILS_LICENSE_FILES = LICENSE LICENSE_RUS
+
+define RUTOKEN_M2M_UTILS_INSTALL_TARGET_CMDS
+	mkdir -p $(RUTOKEN_M2M_UTILS_API_INSTALL_DIR)
+	mkdir -p $(RUTOKEN_M2M_UTILS_README_INSTALL_DIR)
+	$(INSTALL) -D -m 0755 $(@D)/rt-uart-test $(RUTOKEN_M2M_UTILS_INSTALL_DIR)
+	$(INSTALL) -D -m 0755 $(@D)/rt-control $(RUTOKEN_M2M_UTILS_INSTALL_DIR)
+	$(INSTALL) -D -m 0755 $(@D)/rt-run-sample $(RUTOKEN_M2M_UTILS_INSTALL_DIR)
+	
+	$(INSTALL) -D -m 0755 $(@D)/include/rt-api $(RUTOKEN_M2M_UTILS_API_INSTALL_DIR)
+
+	$(INSTALL) -D -m 0644 $(@D)/README.md $(RUTOKEN_M2M_UTILS_README_INSTALL_DIR)
+	$(INSTALL) -D -m 0644 $(@D)/README_RUS.md $(RUTOKEN_M2M_UTILS_README_INSTALL_DIR)
+endef
+
+$(eval $(generic-package))
