a5cad66d11548df900e6bcc79847d1d31b82bb46
Eugene Mironenko <mironenko@rutoken.ru>
2020-03-12 12:08:19 +0300

    Add rtuart package

---
+++ package/rutoken-m2m-sdk/rtuart/rtuart.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# rtuart
+#
+################################################################################
+
+RTUART_VERSION     = testing
+RTUART_SOURCE      = rtuart-$(RTUART_VERSION).tar.gz
+RTUART_SITE_METHOD = git
+RTUART_SITE        = git@scm.aktivco.ru:rutoken-res/m2m-ecp-uart.git
+RTUART_INSTALL_TARGET = YES
+RTUART_LICENSE = BSD-2-Clause
+RTUART_LICENSE_FILES = LICENSE
+
+RTUART_CONF_OPTS = -DRTUART_SERIAL_PORT="/dev/ttyAMA0"
+
+ifeq ($(BR2_PACKAGE_RUTOKEN_UTILS),y)
+	RTUART_CONF_OPTS += -DTRANSPORT_TEST_INSTALL_PATH="/opt/Rutoken/bin"
+endif
+
+$(eval $(cmake-package))
